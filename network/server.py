fb15b994e717c3987d2cdd3ac3ae3ad5159db137
teacher <teacher@learn.org>
2025-04-12 18:42:51 -0400

    indentation

--- network/server.py
+++ network/server.py
@@ -1,33 +1,26 @@
 # server.py
+import socket
+def main():
+    # Create a socket object
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-   import socket
+    # Get local machine name
+    host = 'localhost'
+    port = 12345
 
-   def main():
-       # Create a socket object
-       server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Bind to the port
+    server_socket.bind((host, port))
 
-       # Get local machine name
-       host = 'localhost'
-       port = 12345
+    # Queue up to 5 requests
+    server_socket.listen(5)
+    print(f"Server listening on {host}:{port}")
+    while True:
+        # Establish a connection
+        client_socket, addr = server_socket.accept()
+        print(f"Got a connection from {addr}")
+        message = 'Thank you for connecting'
+        client_socket.send(message.encode('utf-8'))
+        client_socket.close()
 
-       # Bind to the port
-       server_socket.bind((host, port))
-
-       # Queue up to 5 requests
-       server_socket.listen(5)
-
-       print(f"Server listening on {host}:{port}")
-
-       while True:
-           # Establish a connection
-           client_socket, addr = server_socket.accept()
-
-           print(f"Got a connection from {addr}")
-
-           message = 'Thank you for connecting'
-           client_socket.send(message.encode('utf-8'))
-
-           client_socket.close()
-
-   if __name__ == "__main__":
-       main()
+if __name__ == "__main__":
+    main()

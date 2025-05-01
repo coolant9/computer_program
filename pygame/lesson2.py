d156f263c276043b3e180d5dafb80ffe6aa2102b
student <student@school.org>
2025-05-01 16:07:43 -0400

    correct logic for bounce.

--- pygame/lesson2.py
+++ pygame/lesson2.py
@@ -1,8 +1,10 @@
 # Example file showing a circle moving on screen
 import pygame
-def update_player_pos(d,player_pos):
-    player_pos.y += d*10    
-    return player_pos
+def update_player_pos(dy,ball_pos):
+    ball_pos.y += dy*10 
+    ball_pos.x += -1*0.1*10   
+    return ball_pos
+    
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1000,1000))
@@ -10,8 +12,9 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 x = 500
-d = -1
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+dy = -1
+dx =  1
+ball_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 rectangle1_left = 400
 rectangle2_left = 400
 while running:
@@ -23,24 +26,24 @@ while running:
     
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
-    player_pos = update_player_pos(d,player_pos)
-    pygame.draw.circle(screen, "red",player_pos , 40)
+    ball_pos = update_player_pos(dy,ball_pos)
+    pygame.draw.circle(screen, "red",ball_pos , 40)
     
-    if player_pos.y == 50:
-        d = 1
+    def is_hitting(rectangle,ball):
+        center = rectangle.x + rectangle.width/2
+        return abs(center - ball.x) <= 100
     
-    if player_pos.y == 950:
-        d = -1
+    
     rectangle1 = pygame.Rect(rectangle1_left,0, 200, 50)
     rectangle2 = pygame.Rect(rectangle2_left,950, 200, 50)
-    if rectangle1.x <= 300 and player_pos.y<=200:
-        d = -1
-    if rectangle2.x <= 300 and player_pos.y >= 800:
-        d = 1
-    if rectangle1.x >= 600 and player_pos.y <= 200:
-        d = -1
-    if rectangle2.x >= 600 and player_pos.y >= 800:
-        d = 1
+    
+    if ball_pos.y == 50 and is_hitting(rectangle1, ball_pos):
+        dy = 1
+
+    if ball_pos.y == 950 and is_hitting(rectangle2, ball_pos):
+        dy = -1
+  
+   
     
     
 
@@ -52,7 +55,7 @@ while running:
     if keys[pygame.K_a]:
         rectangle1_left -= 300 * dt
     if keys[pygame.K_d]:
-        rectangle1_left += 300 * dt     
+        rectangle1_left += 300 * dt
     if keys[pygame.K_j]:  
         rectangle2_left -= 300 * dt 
     if keys[pygame.K_l]:  

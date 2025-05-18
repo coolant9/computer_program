f92b7aaa53d1f66ee8b25d9f3530872b153b8652
teacher <teacher@learn.org>
2025-05-17 22:09:18 -0400

    make server and client accept command line args.

--- network/client.py
+++ network/client.py
@@ -1,19 +1,24 @@
 # client.py
 
 import socket
+import sys
 
 def main():
    # Create a socket object
    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
    # Connect to the server
-   host = 'tiger'
-   port = 12345
+   host = sys.argv[1] if len(sys.argv) > 1 else 'localhost'
+   port = int(sys.argv[2]) if len(sys.argv) > 2 else 12345
    client_socket.connect((host, port))
+   print(f"Connected to server at {host}:{port}")
 
+   message = "start"
    # Receive data from the server
-   data = client_socket.recv(1024)
-   print(f"Received from server: {data.decode('utf-8')}")
+   while message != "bye":
+      data = client_socket.recv(1024)
+      message = data.decode('utf-8')
+      print(f"Received from server: {message}")
 
    # Close the connection
    client_socket.close()
--- network/server.py
+++ network/server.py
@@ -1,12 +1,13 @@
 # server.py
 import socket
+import sys
 def main():
     # Create a socket object
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Get local machine name
-    host = 'localhost'
-    port = 12345
+    host = sys.argv[1] if len(sys.argv) > 1 else 'localhost'
+    port = sys.argv[2] if len(sys.argv) > 2 else 12345
 
     # Bind to the port
     server_socket.bind((host, port))
